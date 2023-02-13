7097474ea13e5f2a68749e558eaf63e9b36416ff
Fabian Roscher <roscher2003@outlook.de>
2023-02-13 15:40:40 +0100

    added warrior shield, needs still a blocking texture

--- src/main/java/net/mrlolf/vikings/item/ModItems.java
+++ src/main/java/net/mrlolf/vikings/item/ModItems.java
@@ -1,10 +1,10 @@
 package net.mrlolf.vikings.item;
 
+
+import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricShieldItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import net.minecraft.registry.Registry;
@@ -13,7 +13,8 @@ import net.mrlolf.vikings.Vikings;
 
 public class ModItems {
     public static final Item MEAD = registerItem("mead", new Item(new FabricItemSettings()));
-
+    // FabricShieldItem(settings.maxDamage(durability), cooldownTicks, enchantability, repairItem)
+    public static final Item WARRIOR_SHIELD = registerItem("warrior_shield", new FabricShieldItem(new FabricItemSettings().maxDamage(2500), 10, 13, Items.IRON_INGOT));
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Vikings.MODID, name), item);
     }
@@ -22,6 +23,7 @@ public class ModItems {
     }
     public static void addItemsToItemGroup(){
         addToItemGroup(ItemGroups.FOOD_AND_DRINK, MEAD);
+        addToItemGroup(ItemGroups.COMBAT, WARRIOR_SHIELD);
     }
     public static void registerModItems(){
         Vikings.LOGGER.info("Loading Items for " + Vikings.MODID);
